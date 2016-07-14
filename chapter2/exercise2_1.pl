8f2109c725ff16853f45551eee90a944535243b0
Greg Anderson <greg@infobytes.com>
2016-07-14 13:31:51 -0600

    Finished exercise 2.1

--- chapter2/exercise2_1.pl
+++ chapter2/exercise2_1.pl
@@ -1,4 +1,19 @@
 use strict;
 use warnings;
 
+print "Enter number: ";
+my $num = <>;
+chomp($num);
+while ($num =~ /\D/) {
+    print "Invalid input.\n";
+    print "Enter number: ";
+    $num = <>;
+    chomp($num);
+}
 
+print "You entered $num\n";
+
+my $len = length($num);
+for (my $i = 1; $i <= 10; $i++) {
+    printf("%2d * %${len}d = %d\n", $i, $num, $i * $num);
+}
