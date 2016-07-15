713768e76ea8ded6fe1abe749bb1972437ac87c9
Greg Anderson <greg@infobytes.com>
2016-07-15 11:31:35 -0600

    IO::Prompter example

---
+++ io_prompter_example.pl
@@ -0,0 +1,17 @@
+use strict;
+use warnings;
+use IO::Prompter;
+
+my $num = prompt -num, 'Enter a number';
+print "Your number: $num\n";
+
+my $passwd = prompt "Enter your password", -echo=>'*';
+print "Your password: $passwd\n";
+
+my $selection = prompt "Choose wisely...", -menu=> {
+        wealth => [ 'moderate', 'vast', 'incalculable' ],
+        health => [ 'hale', 'hearty', 'rude' ],
+        wisdom => [ 'cosmic', 'folk' ],
+    }, '>';
+
+print "Your selection: $selection\n";
